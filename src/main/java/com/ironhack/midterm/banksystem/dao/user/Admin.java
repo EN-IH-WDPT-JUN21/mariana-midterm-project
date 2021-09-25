174fc674235bc699264bb23aa4e5097dd83dc66b
MarianaFariaa <marianaefaria@gmail.com>
2021-09-25 13:13:45 +0100

    Added Admin class

---
+++ src/main/java/com/ironhack/midterm/banksystem/dao/user/Admin.java
@@ -0,0 +1,16 @@
+package com.ironhack.midterm.banksystem.dao.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Entity
+@Setter
+@Table(name="admin")
+public class Admin extends User{
+}
